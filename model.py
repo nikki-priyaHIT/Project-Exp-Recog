93d743e1324082e67d0c329da930c418b36c70ca
Umang Tank <umangtank08@gmail.com>
2022-03-26 14:08:15 +0530

    Logging module added

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.models import model_from_json
 from tensorflow.python.keras.backend import set_session
 import numpy as np
-
+import logging
 import tensorflow as tf
 
 config = tf.compat.v1.ConfigProto()
@@ -11,7 +11,7 @@ set_session(session)
 
 
 class FacialExpressionModel(object):
-
+    logging.info("Model loaded successfully")
     EMOTIONS_LIST = ["Angry", "Disgust",
                      "Fear", "Happy",
                      "Neutral", "Sad",
@@ -19,17 +19,21 @@ class FacialExpressionModel(object):
 
     def __init__(self, model_json_file, model_weights_file):
         # load model from JSON file
+        logging.info("Loading model from JSON file")
         with open(model_json_file, "r") as json_file:
             loaded_model_json = json_file.read()
             self.loaded_model = model_from_json(loaded_model_json)
-
+            logging.info("Model loaded successfully")
         # load weights into the new model
         self.loaded_model.load_weights(model_weights_file)
         #self.loaded_model.compile()
         #self.loaded_model._make_predict_function()
 
     def predict_emotion(self, img):
+        logging.info("Predicting emotion")
         global session
         set_session(session)
+        logging.info("Session set")
         self.preds = self.loaded_model.predict(img)
+        logging.info("Prediction done")
         return FacialExpressionModel.EMOTIONS_LIST[np.argmax(self.preds)]
